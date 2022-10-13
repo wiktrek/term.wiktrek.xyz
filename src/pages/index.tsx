ac58f855eaf6e4abb06fda410942b628c9047a8b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-10-13 23:06:07 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Info - wiktrek.xyz</title>
+        <meta name="description" content="info" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className=""></main>
+    </>
+  );
+};
+
+export default Home;
