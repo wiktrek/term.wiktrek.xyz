b7771ebecac780f4b726c361769db593f9ec553b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-12-06 17:04:32 +0100

    termianal input modified

--- src/pages/terminal.tsx
+++ src/pages/terminal.tsx
@@ -15,12 +15,22 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="left-10">
         <Navbar />
         <div className="text-emerald-500">
-          <a>test</a>
-          <input type="text" value={input} onChange={handleChange} />
-          <input className="border-none bg-transparent focus:border-none active:border-none" />
+          <a className="text-orange-500">guest</a>
+          <a className="text-yellow-500">@</a>
+          <a className="text-green-600">info.wiktrek.xyz</a>
+          <a className="text-yellow-500">:$ ~</a>
+          <input
+            type="text"
+            value={input}
+            onChange={handleChange}
+            className="border-none bg-transparent focus:ring-0"
+          />
+        </div>
+        <div>
+          <a>{input}</a>
         </div>
       </main>
     </>
